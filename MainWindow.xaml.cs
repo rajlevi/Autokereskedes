1b4011574f15ceabc1e3d0ef7c0c259accb04c98
rajlevi <rajlevix11@gmail.com>
2025-04-29 17:54:50 +0200

    adatbazis keszuloben

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Autoker;
+
 
 namespace Autokereskedes
 {
@@ -16,10 +20,49 @@ namespace Autokereskedes
     /// </summary>
     public partial class MainWindow : Window
     {
+        cnAutoker cn;
         public MainWindow()
         {
             InitializeComponent();
-            MainFrame.Navigate(new LoginPage());
+            cn = new cnAutoker();
+            DBInic();
+            // MainFrame.Navigate(new LoginPage());
+        }
+        private void DBInic()
+        {
+            KezdőAdatok();
+            /*cn.Database.EnsureCreated();
+            if (cn.Kereskedes == null) return;
+            if (!cn.Kereskedes.Any())
+            {
+                KezdőAdatok();
+            }*/
+            AdatKiír();
+        }
+        private void KezdőAdatok()
+        {
+            var c1 = new Cim { Varos = "Kecskemét", Utca = "Izsáki út", Hazszam = "10" };
+            var k1 = new Kereskedes { Nev = "JoKocsi", Jegyzekszam = "123-124-123" };
+            c1.Kereskedes=k1;
+            var e1 = new Elado { Nev = "Kis Pista", Telszam = "+36303527532", Szuldatum = "1989.10.05" };
+            e1.Kereskedes1=k1;
+            var a1 = new Auto { Marka = "BMW", Kivitel = "coupe", Evjarat = "2006", Uzemanyag = "Benzin", Szin = "Szürke" };
+            a1.Kereskedes1 = k1;
+            cn.Cims.Add(c1);
+            cn.Kereskedes.Add(k1);
+            cn.Autok.Add(a1);
+            cn.Eladok.Add(e1);
+            cn.SaveChanges();
+        }
+        private void AdatKiír()
+        {
+            var s = "";
+            foreach (var p in
+                cn.Kereskedes.Include(pe => pe.Cim).Include(pe => pe.Autos).Include(pe => pe.Elados).ToList())
+            {
+                s += p.Nev;
+            }
+            MessageBox.Show(s);
         }
     }
 }
